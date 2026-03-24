c0bb69e246910ffb95c0b7afc2e3f08e0ea38961
hun2hun2 <aa09302003@gmail.com>
2026-03-24 21:05:47 +0900

    fix .gitignore

--- MyShooter/Intermediate/Build/Win64/MyShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.44.35223
-#include "D:/UE_5.0.3_Projects/MyShooter/Intermediate/Build/Win64/MyShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Pangaea/Intermediate/Build/Win64/PangaeaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.44.35224
-#include "D:/UE_5.0.3_Projects/Pangaea/Intermediate/Build/Win64/PangaeaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
